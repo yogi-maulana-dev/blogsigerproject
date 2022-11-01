79b18e722fa3ecedc84d2e40638b4e521112c5b6
Ays1234 <yogimaulana100@gmail.com>
2022-11-02 02:21:23 +0700

    login php

---
+++ backend/views/layouts/hahkosong.php
@@ -0,0 +1,50 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var string $content */
+
+use backend\assets\AppAsset;
+use yii\helpers\Html;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>" class="h-100">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    	<link rel="icon" href="img/icon.ico" type="image/x-icon"/>
+
+	<!-- Fonts and icons -->
+	<script src="js/plugin/webfont/webfont.min.js"></script>
+	<script>
+		WebFont.load({
+			google: {"families":["Lato:300,400,700,900"]},
+			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['css/fonts.min.css']},
+			active: function() {
+				sessionStorage.fonts = true;
+			}
+		});
+	</script>
+
+	<!-- CSS Files -->
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="">
+    <?php $this->head() ?>
+</head>
+<body class="d-flex flex-column h-100">
+<?php $this->beginBody() ?>
+
+<main role="main">
+    <div class="container">
+        <?= $content ?>
+    </div>
+</main>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage();
