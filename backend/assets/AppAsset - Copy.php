ca2dc53d39cedeed64343b08964f49b88b2b3451
Ays1234 <yogimaulana100@gmail.com>
2022-12-02 21:46:04 +0700

    berita

---
+++ backend/assets/AppAsset - Copy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main backend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/bootstrap.min.css',
+        'css/atlantis.css',
+    ];
+    public $js = [
+          'js/core/jquery.3.2.1.min.js',
+        'js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js',
+        'js/core/popper.min.js',
+        'js/core/bootstrap.min.js',
+        'js/atlantis.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap5\BootstrapAsset',
+    ];
+}
