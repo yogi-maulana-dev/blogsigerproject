daac504a7c28b79f08997f5a93b4aea2d81fb81c
yogi-maulana-dev <yogimaulana100@gmail.com>
2023-04-20 17:58:13 +0700

    add css

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,10 +12,14 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
         'css/style.css',
     ];
     public $js = [
+          'js/core/jquery.3.2.1.min.js',
+        'js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js',
+        'js/core/popper.min.js',
+        'js/core/bootstrap.min.js',
+        'js/atlantis.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
