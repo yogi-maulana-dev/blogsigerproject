f12312d1d02c2189e45575d9f5d4ff56404e79a9
yogi-maulana-dev <yogimaulana100@gmail.com>
2023-04-23 10:09:56 +0700

    add layout main

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -7,7 +7,7 @@ use backend\assets\AppAsset;
 use common\widgets\Alert;
 use yii\bootstrap5\Breadcrumbs;
 use yii\bootstrap5\Html;
-use yii\bootstrap5\Nav;
+// use yii\bootstrap5\Nav;
 use yii\bootstrap5\NavBar;
 
 AppAsset::register($this);
@@ -27,23 +27,23 @@ AppAsset::register($this);
 
 <header>
     <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
-        ],
-    ]);
+    // NavBar::begin([
+    //     'brandLabel' => Yii::$app->name,
+    //     'brandUrl' => Yii::$app->homeUrl,
+    //     'options' => [
+    //         'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
+    //     ],
+    // ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     }     
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
-        'items' => $menuItems,
-    ]);
+    // echo Nav::widget([
+    //     'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
+    //     'items' => $menuItems,
+    // ]);
     if (Yii::$app->user->isGuest) {
         echo Html::tag('div',Html::a('Login',['/site/login'],['class' => ['btn btn-link login text-decoration-none']]),['class' => ['d-flex']]);
     } else {
@@ -54,7 +54,7 @@ AppAsset::register($this);
             )
             . Html::endForm();
     }
-    NavBar::end();
+    // NavBar::end();
     ?>
 </header>
 
